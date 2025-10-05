6f9daf65444f7c2d86da4dc9282540532d644f1f
Romano Romano <romano.romano@gmail.com>
2025-10-05 11:48:49 +0200

    feat: add Organizational Units

--- main.tf
+++ main.tf
@@ -1,3 +1,28 @@
 resource "aws_organizations_organization" "this" {
   feature_set = "ALL"
 }
+
+resource "aws_organizations_organizational_unit" "security" {
+  name      = "Security"
+  parent_id = aws_organizations_organization.this.roots[0].id
+}
+
+resource "aws_organizations_organizational_unit" "production" {
+  name      = "Production"
+  parent_id = aws_organizations_organization.this.roots[0].id
+}
+
+resource "aws_organizations_organizational_unit" "non_production" {
+  name      = "Non-Production"
+  parent_id = aws_organizations_organization.this.roots[0].id
+}
+
+resource "aws_organizations_organizational_unit" "sandbox" {
+  name      = "Sandbox"
+  parent_id = aws_organizations_organization.this.roots[0].id
+}
+
+resource "aws_organizations_organizational_unit" "shared_services" {
+  name      = "Shared Services"
+  parent_id = aws_organizations_organization.this.roots[0].id
+}
