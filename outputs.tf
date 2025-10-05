7b5bf9735bf281181f49b72d6c023417e9b0ab2d
Romano Romano <romano.romano@gmail.com>
2025-10-05 17:41:42 +0200

    feat: add IAM Identity Center outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,13 @@ output "organizations_organization_id" {
   description = "Organization ID"
   value       = aws_organizations_organization.this.id
 }
+
+output "ssoadmin_instances_arns" {
+  description = "IAM Identity Center instance ARN"
+  value       = local.sso_instance_arn
+}
+
+output "ssoadmin_instances_identity_store_ids" {
+  description = "IAM Identity Center Identity Store ID"
+  value       = local.identity_store_id
+}
