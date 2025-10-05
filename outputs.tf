afed0787ed372b53efdab12d3b74bc34f2c728a0
Romano Romano <romano.romano@gmail.com>
2025-10-05 18:08:46 +0200

    docs: move the comment header to the correct position

--- outputs.tf
+++ outputs.tf
@@ -4,12 +4,12 @@ output "organizations_organization_id" {
   value       = aws_organizations_organization.this.id
 }
 
+# IAM Identity Center (SSO)
 output "ssoadmin_instances_arns" {
   description = "IAM Identity Center instance ARN"
   value       = local.sso_instance_arn
 }
 
-# IAM Identity Center (SSO)
 output "ssoadmin_instances_identity_store_ids" {
   description = "IAM Identity Center Identity Store ID"
   value       = local.identity_store_id
